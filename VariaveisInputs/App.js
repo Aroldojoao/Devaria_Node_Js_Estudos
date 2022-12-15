cd8e13679d81506f132a31ecfd6290b0b16bd650
Aroldo de Souza Joao <Aroldo.joao@hotmail.com>
2022-12-14 22:33:51 -0300

    HelloWord

--- VariaveisInputs/App.js
+++ VariaveisInputs/App.js
@@ -7,7 +7,7 @@ const minhaPrimeiraConstanteString = 'Minha primeira constante';
 console.log (minhaPrimeiraConstanteString);
 
 let leituraDeCampo;
-readLine.question('Digite sua idade:', input => {
+readLine.question('Digite seu nome:', input => {
 leituraDeCampo = input;
 console.log(`O usuario digitou: ${leituraDeCampo}`);
 });
